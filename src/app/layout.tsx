0243cd5c23fd7e216d16d0d6cb54da219c2535fe
Adam P <adam.porbanderwalla@gmail.com>
2026-03-23 21:15:03 -0400

    fix: changed max scale and user scalable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,8 @@ const jetbrainsMono = JetBrains_Mono({
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export const metadata: Metadata = {
